667d7d3fa530bc4dae18dd6dbb408dd3799ec76b
Chase Carter <1781966+Chasec98@users.noreply.github.com>
2025-01-17 01:16:02 -0500

    all pages, optimize images, add more content, another font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ export default {
 				survivor: ['Survivor', 'sans-serif'],
 				chelsea: ['Chelsea Market', 'sans-serif'],
 				fredericka: ['Fredericka', 'sans-serif'],
+				museo: ['Museo', 'sans-serif'],
 			},
 			brightness: {
 				35: '.35',
