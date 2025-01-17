435fa9bb3aa5bf8c0c0791a64624747e498eafd6
Chase Carter <1781966+Chasec98@users.noreply.github.com>
2025-01-16 22:40:44 -0500

    fix gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chasec98.github.io',
+  base: 'survivor-gr',
   integrations: [tailwind({
     applyBaseStyles: false,
   })]
