617469e228a5364ea36f1d3c03eae91a6c51e3cb
Chase Carter <1781966+Chasec98@users.noreply.github.com>
2025-01-16 22:27:32 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				survivor: ['Survivor', 'sans-serif'],
+				chelsea: ['Chelsea Market', 'sans-serif'],
+				fredericka: ['Fredericka', 'sans-serif'],
+			},
+			brightness: {
+				35: '.35',
+			}
+		},
+	},
+	plugins: [],
+}
